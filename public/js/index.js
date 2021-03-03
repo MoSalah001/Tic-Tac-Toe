b9ba98a8e86d2e7f715d28fd6d2e46e4c5991eb8
MoSalah001 <mhmdsalah707@gmail.com>
2021-03-03 20:20:22 +0200

    action func

--- public/js/index.js
+++ public/js/index.js
@@ -1,12 +1,23 @@
 const divs = document.querySelectorAll(".game");
 
+let img = document.createElement("img")
+
 const player1 = {
     win : 0,
     lose : 0,
-    sign : "../Mat/circle.svg"
+    sign : "public/Mat/circle.svg"
 }
 
+const player2 = {
+    win : 0,
+    lose : 0,
+    sign : "public/Mat/cross.svg"
+}
+
+
 function action() {
+    img.src=`${player1.sign}`
+    this.appendChild(img);
 }
 
 for(i in divs) {
