d2b680f24dfad6c3bab1dd93d36fd29fbfa54e33
MoSalah001 <mhmdsalah707@gmail.com>
2021-03-22 18:14:45 +0200

    resetting the game after win and draw

--- public/js/index.js
+++ public/js/index.js
@@ -6,13 +6,15 @@ const circle = "public/Mat/circle.svg"
 
 const cross = "public/Mat/cross.svg"
 
-const player1 = {
-    win : 0,
-    lose : 0,
-    sign : "public/Mat/circle.svg"
-}
+const resetbtn = document.querySelector("#reset")
 
 const player2 = {
+    win : 0,
+    lose : 0,
+    sign : "public/Mat/circle.svg",
+}
+
+const player1 = {
     win : 0,
     lose : 0,
     sign : "public/Mat/cross.svg"
@@ -20,11 +22,14 @@ const player2 = {
 
 const track = [];
 
-for(let i = 0; i< divs.length; i++) {
-    divs[i].addEventListener('click', action);
-    let x = document.createElement("img");
-    divs[i].append(x);
+function play(){
+    for(let i = 0; i< divs.length; i++) {
+        divs[i].addEventListener('click', action);
+        let x = document.createElement("img");
+        divs[i].append(x);
+    }
 }
+play()
 
 function action() {
     if(this.firstChild.src === "") {
@@ -44,7 +49,6 @@ function check() {
             let host = "http://127.0.0.1:5500/";
             let crs = host+cross;
             let crc = host+circle;
-        
             if( // cross wins  player 2
                 (divs[0].firstChild.src === crs && divs[1].firstChild.src === crs && divs[2].firstChild.src === crs) || 
                 (divs[3].firstChild.src === crs && divs[4].firstChild.src === crs && divs[5].firstChild.src === crs) || 
@@ -55,8 +59,12 @@ function check() {
                 (divs[0].firstChild.src === crs && divs[4].firstChild.src === crs && divs[8].firstChild.src === crs) ||
                 (divs[2].firstChild.src === crs && divs[4].firstChild.src === crs && divs[6].firstChild.src === crs)
             ){
-                console.log("win");
-        } else if( // circle win player 1
+                player1.win +=1;
+                player2.lose +=1;
+                document.querySelector("#win1").innerHTML = `Score: ${player1.win}`;
+                resetbtn.style.display= "block";
+                
+            } else if( // circle win player 1
                 (divs[0].firstChild.src === crc && divs[1].firstChild.src === crc && divs[2].firstChild.src === crc) || 
                 (divs[3].firstChild.src === crc && divs[4].firstChild.src === crc && divs[5].firstChild.src === crc) || 
                 (divs[6].firstChild.src === crc && divs[7].firstChild.src === crc && divs[8].firstChild.src === crc) ||
@@ -66,9 +74,24 @@ function check() {
                 (divs[0].firstChild.src === crc && divs[4].firstChild.src === crc && divs[8].firstChild.src === crc) ||
                 (divs[2].firstChild.src === crc && divs[4].firstChild.src === crc && divs[6].firstChild.src === crc)
             ){
-                player1.win +=1;
-                player2.lose +=1   ; 
-                console.log(player1.win);
-    }
+                player2.win +=1;
+                player1.lose +=1;   ; 
+                document.querySelector("#win2").innerHTML = `Score: ${player1.win}`;
+                resetbtn.style.display= "block";
+            } else if(counter % 9 == 0){
+                resetbtn.style.display= "block";
+            }
 }
 
+document.querySelector("#reset").addEventListener('click',reset)
+
+
+function reset(){
+    for(let i = 0; i< divs.length; i++) {
+        divs[i].removeChild(divs[i].firstChild);
+        counter=0;
+    }
+    resetbtn.style.display= "none";
+    play();
+}
+            
